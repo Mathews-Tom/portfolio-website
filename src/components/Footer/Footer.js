6a3dbe5883e3183cfa05c6a2ed38f05b5169d67b
Tom Mathews <9562152+Mathews-Tom@users.noreply.github.com>
2022-07-29 23:59:25 +0530

    Update portfolio for personal use

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+91-62359-13350">+91-62359-13350</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:tom.mathews@nyu.edu">tom.mathews@nyu.edu</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Inovating one project at a time.</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Mathews-Tom">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/mathews-tom/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://twitter.com/tom_mathews">
+            <AiFillTwitterCircle size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.instagram.com/treasuring_momentz/">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi there 👋, I'm <br/>
+        Tom Mathews
+      </SectionTitle> 
+      <SectionText>
+        Experienced Software Developer In Test with a demonstrated history of working in the internet industry. Experienced in designing and developing automation frameworks from the scratch to validate different customer facing products. Actively developing and improving skills in the Machine Learning and Artificial Intelligence field.
+      </SectionText>
+      <Button onClick={() => window.location = "https://google.com"}> Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
